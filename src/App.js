bca8f3eb600f56a998c65d946bd7ecf96cfececa
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2021-10-03 19:48:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Context } from './contextTut/Context';
-import Display from './Display';
 
 const App = () => {
 
-  const personName = "Javohir Hakimov";
-
   return (
-    <>
-      <Context.Provider value={personName}>
-        <Display />
-      </Context.Provider>
-    </>
+    <div>
+      
+    </div>
   );
 }
 export default App;
